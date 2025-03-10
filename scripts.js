c7a60de793475f8bc16fe91bfde9d1f5930d8ba2
Nelly Perkins <132894250+nperkinsr@users.noreply.github.com>
2025-03-10 17:03:13 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,16 +1,14 @@
-window.onload = function () {
-  var allDice = [
-    "./dice1.png",
-    "./dice2.png",
-    "./dice3.png",
-    "./dice4.png",
-    "./dice5.png",
-    "./dice6.png",
-  ];
+var allDice = [
+  "images/dice1.png",
+  "images/dice2.png",
+  "images/dice3.png",
+  "images/dice4.png",
+  "images/dice5.png",
+  "images/dice6.png",
+];
 
-  var outcome1 = allDice[Math.floor(Math.random() * allDice.length)];
-  var outcome2 = allDice[Math.floor(Math.random() * allDice.length)];
+var outcome1 = allDice[Math.floor(Math.random() * allDice.length)];
+var outcome2 = allDice[Math.floor(Math.random() * allDice.length)];
 
-  document.querySelector(".img1").setAttribute("src", outcome1);
-  document.querySelector(".img2").setAttribute("src", outcome2);
-};
+document.querySelector(".img1").setAttribute("src", outcome1);
+document.querySelector(".img2").setAttribute("src", outcome2);
